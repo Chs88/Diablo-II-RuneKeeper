058dfe23b56708b3f2dd9fd96ea7256ac2d57cac
Chs88 <faskertikr@gmail.com>
2020-11-19 21:50:49 +0000

    Query class now gets the save file objects created on init

--- logic.py
+++ logic.py
@@ -11,25 +11,27 @@ import json
 
 class Query():
     def __init__(self, is_plugy_added, is_shared_added, char_path, plugy_path, shared_path):
-        self.is_plugy_added = is_plugy_added
-        self.is_shared_added = is_shared_added
-        self.char_path = char_path
-        self.plugy_path = plugy_path
-        self.shared_path = shared_path
-    
+        self.d2s_file = D2SFile(char_path)
+        if is_plugy_added == True:
+            self.d2x_file = D2XFile(plugy_path)
+        if is_shared_added == True:
+            self.sss_file = SSSFile(shared_path)    
 
     def load(self):
-        d2s_file = D2SFile(self.char_path)
-        if self.is_plugy_added == True:
-            d2x_file = D2XFile(self.plugy_path)
-        if self.is_shared_added == True:
-            sss_file = SSSFile(self.shared_path)
-        print(d2s_file.char_name, d2s_file.char_level)
+        instance = FindRunes()
+        instance.find()
 
 
 
 
 # class FindRunes():
+#     def __init__(self):
+#         print("test")
+    
+#     def find(self):
+#         print("wegothere")
+
+
 
 
 
