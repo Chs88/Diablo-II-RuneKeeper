af4fe79307046394626174e8f19ec7b7b62fa880
Chs88 <faskertikr@gmail.com>
2020-11-20 01:05:46 +0000

    Tested rune listing functions in logic.py

--- logic.py
+++ logic.py
@@ -22,11 +22,12 @@ else:
 
         def load(self):
             instance = FindRunes()
-            instance.list_runewords_inv(self.d2s_file)
-            instance.list_runewords_stash(self.d2x_file)
-            instance.list_runewords_shared(self.sss_file)
+            instance.list_runes_inv(self.d2s_file)
+            instance.list_runes_stash(self.d2x_file)
+            # instance.list_runes_shared(self.sss_file)
             instance.sum_runes()
             print(instance.runes_total)
+            # instance.list_all(self.d2x_file)
 
 
 
@@ -39,18 +40,18 @@ else:
             self.runes_total = []
 
         
-        def list_runewords_inv(self, d2s_file):
+        def list_runes_inv(self, d2s_file):
             for item in d2s_file.items:
                 if item.itype == 3 and "Rune" in item.name:
                     self.runes_in_inventory.append(item.name[:-5])
 
-        def list_runewords_stash(self, d2x_file):
+        def list_runes_stash(self, d2x_file):
             for page in d2x_file.stash:
                 for item in page['items']:
                     if item.itype == 3 and "Rune" in item.name:
                         self.runes_in_stash.append(item.name[:-5])     
         
-        def list_runewords_shared(self, sss_file):
+        def list_runes_shared(self, sss_file):
             for page in sss_file.stash:
                 for item in page['items']:
                     if item.itype == 3 and "Rune" in item.name:
@@ -63,5 +64,13 @@ else:
             self.runes_total.extend(self.runes_in_shared)
 
 
+        def list_all(self, d2x_file):
+            for page in d2x_file.stash:
+                for item in page['items']:
+                    print(item.level)
+                    print(item.name)
+                    print(item.base_name)
+                    print(item.magic_attrs)
+            
 
 
