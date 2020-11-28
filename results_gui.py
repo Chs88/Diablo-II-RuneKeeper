f87574cf51b76d203079c53620a0bae1215f5bcc
Chs88 <faskertikr@gmail.com>
2020-11-28 04:45:36 +0000

    Results window is now displaying dummy text and is read only

--- results_gui.py
+++ results_gui.py
@@ -7,8 +7,9 @@ class ResultsWindow(tk.Frame):
         self.parent = parent
         
         ##Declaring widgets
-        self.title_label = tk.Label(self.parent, text="Results Page")
-        
+        self.scrollbar = tk.Scrollbar(self.parent)
+        self.text_window = tk.Text(self.parent, height=50, width=50,state='disabled')
+        self.test_text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
 
 
 
@@ -17,7 +18,20 @@ class ResultsWindow(tk.Frame):
 
         ##Rendering widgets
         #Row 0
-        self.title_label.grid(row=0, column=0, padx=100, pady=10, sticky=tk.N+tk.W+tk.E+tk.S, columnspan=2, )
+        self.text_window.pack(side=tk.LEFT, fill=tk.Y)
+        self.scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+        self.scrollbar.config(command=self.text_window.yview)
+        self.text_window.config(yscrollcommand=self.scrollbar.set)
+        self.toggle_read_only()
+        self.text_window.insert(tk.END, self.test_text)
+        self.toggle_read_only()
+
+
+    def toggle_read_only(self):
+        if self.text_window.cget('state') == 'disabled':
+            self.text_window.config(state='normal')
+        elif self.text_window.cget('state') == 'normal':
+            self.text_window.config(state='disabled')
 
 
         
